0d83569f65eff18cff9f410c491c94a42bc75b55
sam <nullvoidnil@mail.com>
2018-01-23 23:28:49 -0500

    set up app

--- src/router_app.erl
+++ src/router_app.erl
@@ -13,9 +13,27 @@
 %%====================================================================
 %% API
 %%====================================================================
+start_deps() ->
+  application:start(sasl),	
+  application:start(crypto),
+  application:start(cowlib),
+  application:start(ranch),
+  application:start(cowboy),
+  application:ensure_all_started(hackney),
+  Options = [{timeout, 150000}, {max_connections, 200}],
+  hackney_pool:start_pool(router_pool, Options).
+  
 
 start(_StartType, _StartArgs) ->
-    router_sup:start_link().
+  start_deps(),
+  ets:new(procs, [set, named_table, public]),
+  Dispatch = cowboy_router:compile([
+    {'_', [{"/store/:id", store_proxy, []}]} 
+  ]),
+  {ok, _} = cowboy:start_clear(http, [{port, 8080}], #{ 
+    env => #{dispatch => Dispatch} }
+  ),
+  router_sup:start_link(). 
 
 %%--------------------------------------------------------------------
 stop(_State) ->
